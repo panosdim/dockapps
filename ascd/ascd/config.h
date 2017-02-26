ab55306d55e6c02ea4475afa52ed894c2c326d26
Doug Torrance <dtorrance@piedmont.edu>
2017-02-25 22:14:00 -0500

    ascd: Remove autogenerated config.h from git.

--- ascd/ascd/config.h
+++
@@ -1,6 +0,0 @@
-#define PACKAGE "ascd"
-#define DEFAULT_COLOR "#2FAFAF"
-#define DEFAULT_BGCOLOR "#000000"
-#define DEFAULTDEVICE "/dev/cdrom"
-#define THDIR "/usr/local/share/AScd"
-#define VERSION 		"0.13.2"
